6c24a7bd9289515ad78eea9faefc784cc38c6fea
falilou <falloufall2006@icloud.com>
2026-06-29 20:46:34 +0000

    Mise à jour membre3.py

--- Membre3.py
+++ Membre3.py
@@ -1,34 +1,29 @@
-production_ferme = {
-    "Mil": 1500,
-    "Mais": 800,
-    "Riz": 1200,
-    "Arachide": 600
-}
-#.Fonction pour calculer la production totale
-def production_totale(donnees_production): 
-    """
-    calculer la somme de toutes les productions.
-    """
-    total = sum(donnees_production.values())
+from membre1 import producteurs
+from membre2 import PRODUITS_VALIDES 
+
+def production_totale():
+    """Calcule et affiche la production totale."""
+    total = 0.0
+    
+    for p in producteurs:
+        for r in p["recoltes"]:
+            total += r["quantite"]
+            
+    print(f"La production totale est : {total} kg")
     return total
-#.Fonction pour afficher la production par produit
-def production_par_produit(donnees_production):
-    """
-    Parcourt le dictionnaire pour afficher le detail de chaque produit.
-    """
-    print("\n--- Détail de la production par produit ---")
-    for produit, quantite in donnees_production.items():
-        print(f"- {produit} : {quantite} kg")
 
-#=================================================
-#EXECUTION DU PROGRAMME
-#=================================================
-
-#.Appel de la fonction bonus pour afficher le détail
-production_par_produit(production_ferme)
-
-#.Appel de la fonction pour le total et stockage du résultat
-total_kg = production_totale(production_ferme)
-
-print("\n--- Résultat Globale ---")  
-print(f"Production totale de la ferme es de : {total_kg} kg")
+def production_par_produit():
+    """Calcule et affiche la production par produit."""
+    stats_produits = {}
+    
+    for p in producteurs:
+        for r in p["recoltes"]:
+            nom_produit = r["produit"]
+            if nom_produit in stats_produits:
+                stats_produits[nom_produit] += r["quantite"]
+                
+    print("\n--- Production par Produit ---")
+    for produit, quantite in stats_produits.items():
+        print(f"{produit} : {quantite} kg")
+        
+    return stats_produits
